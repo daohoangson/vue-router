245c567497d4f674eb8a95cee138b3c4f3eb5dfc
Dao Hoang Son <daohoangson@gmail.com>
2017-05-06 00:20:27 +0700

    [build] 2.5.3-daohoangson

--- dist/vue-router.esm.js
+++ dist/vue-router.esm.js
@@ -1,5 +1,5 @@
 /**
-  * vue-router v2.5.3
+  * vue-router v2.5.3-daohoangson
   * (c) 2017 Evan You
   * @license MIT
   */
@@ -2192,9 +2192,11 @@ function pushHash (path) {
 }
 
 function replaceHash (path) {
-  var i = window.location.href.indexOf('#');
+  var href = window.location.href;
+  var i = href.indexOf('#');
+
   window.location.replace(
-    window.location.href.slice(0, i >= 0 ? i : 0) + '#' + path
+    window.location.href.slice(0, i >= 0 ? i : href.length) + '#' + path
   );
 }
 
@@ -2455,7 +2457,7 @@ function createHref (base, fullPath, mode) {
 }
 
 VueRouter.install = install;
-VueRouter.version = '2.5.3';
+VueRouter.version = '2.5.3-daohoangson';
 
 if (inBrowser && window.Vue) {
   window.Vue.use(VueRouter);
